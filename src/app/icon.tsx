b31703119a66ed21e87a3cc019565e70096f296d
Dara Sovann <daraofficial179@gmail.com>
2026-06-21 02:16:55 +0700

    Add favicon

---
+++ src/app/icon.tsx
@@ -0,0 +1,34 @@
+import { ImageResponse } from "next/og";
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "#6d28d9",
+          color: "white",
+          fontSize: 20,
+          fontWeight: "bold",
+        }}
+      >
+        AI
+      </div>
+    ),
+    {
+      width: 32,
+      height: 32,
+    }
+  );
+}
